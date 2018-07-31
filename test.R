738a4e6de71fd2c28f38bb470340f4819b4abd65
MAGALLANESJoseManuel <jmagallanes@pucp.edu.pe>
2018-07-30 22:23:31 -0500

    update prepro

--- test.R
+++ test.R
@@ -3,11 +3,20 @@ new=testOrd
 new$FreedomintheWorld
 
 library(foreign)
-new=read.dta("testST.dta")
-str(new)
+new=read.csv("indexes.csv",
+             colClasses = c("character","numeric",
+                            rep("factor",4)))
 
+
+
+str(new)
+max(new$EconomicFreedom)
 library(haven)
 
-new2=read_dta("testST.dta")
+
+new2=read_dta("indexes.dta")
 str(new2)
 head(new2)
+attr(new2$WorldFreedom)
+attr(new2$WorldFreedom, "names") 
+max(new2$WorldFreedom)
